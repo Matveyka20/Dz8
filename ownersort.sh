c625c44b77f446b64a76b5bdfa0a69f3b61d6af8
Matveyka20 <141415064+Matveyka20@users.noreply.github.com>
2024-04-10 15:11:56 +0500

    Update ownersort.sh

--- ownersort.sh
+++ ownersort.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
-forall=/home/Scripts
-
-for file in "$forall"/*
-do
-  if [ -d "$file" ]; then
-    continue
-  fi
-  if [ -e "$file" ]; then
-    owner=$(stat -c %U -- "$file")
-    cp -n "$file" "$forall/$owner"/
-    chown "$owner" "$forall/$owner/$file"
-  fi
+forall=/home/anastasia/Scripts
+cd $forall
+for file in $forall
+ do
+        if [ -d $file ]
+          then
+                continue
+          fi
+          if [ -e $file ]
+          then
+                owner='ls -l $file | grep "^-" | awk {'print $£3'}'
+                $file=$owner*   
+                cp $file $forall/$owner
+                chown $owner $forall/$owner/$file
+        fi
 done
